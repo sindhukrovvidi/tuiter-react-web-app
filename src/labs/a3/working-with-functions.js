9f1d5f152e58e6f52b9f7401baf058eef9c87143
Sindhu Krovvidi <sindhukrovvidi@Sindhus-MacBook-Air.local>
2023-05-26 17:53:49 -0400

    Basic JS concepts

---
+++ src/labs/a3/working-with-functions.js
@@ -0,0 +1,17 @@
+import ES5Functions from "./es5-functions";
+import ArrowFunctions from "./arrow-functions";
+import ImpliedReturns from "./implied-return";
+import FunctionParenthesisAndParameters from "./function-parenthesis-and-parameters";
+
+function WorkingWithFunctions() {
+  return (
+    <div>
+      <ES5Functions />
+      <ArrowFunctions />
+      <ImpliedReturns />
+      <FunctionParenthesisAndParameters />
+    </div>
+  );
+}
+
+export default WorkingWithFunctions;
