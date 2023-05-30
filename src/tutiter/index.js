54a0bdda2f1d4ed7d58be836f42081435f8d9c45
Sindhu Krovvidi <sindhukrovvidi@Sindhus-MacBook-Air.local>
2023-05-30 09:31:49 -0400

    fixed all warnings

--- src/tutiter/index.js
+++ src/tutiter/index.js
@@ -1,11 +1,28 @@
+import { Routes, Route } from "react-router";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+
 
 function Tuiter() {
-  return (
-    <div>
-      <Nav/>
-      <h1>Tuiter</h1>
-    </div>
-  );
+ return (
+   <div>
+     <Nav />
+     <div className="row">
+       <div className="col-2">
+         <NavigationSidebar />
+       </div>
+       <div className="col-7">
+         <Routes>
+           <Route path="/home" element={<h1>Home</h1>} />
+           <Route path="/explore" element={<h1>Explore</h1>} />
+           <Route path="/notifications" element={<h1>Notifications</h1>}/>
+         </Routes>
+       </div>
+       <div className="col-3">
+         <h1>Who to follow</h1>
+       </div>
+     </div>
+   </div>
+ );
 }
 export default Tuiter;
