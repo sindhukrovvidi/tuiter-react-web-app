f95181e784f568b7e3bcbdbf659a824af634c47f
Sindhu Krovvidi <sindhukrovvidi@Sindhus-MacBook-Air.local>
2023-05-30 16:15:11 -0400

    JavaScript and React.js Assignment 3

--- src/tutiter/index.js
+++ src/tutiter/index.js
@@ -24,6 +24,10 @@ function Tuiter() {
             <Route path="/explore" element={<ExploreScreen />} />
             <Route path="/bookmarks" element={<BookmarksScreen />} />
             <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/messages" element={<h1>Messages</h1>} />
+            <Route path="/notifications" element={<h1>Notifications</h1>} />
+            <Route path="/lists" element={<h1>Lists</h1>} />
+            <Route path="/more" element={<h1>More</h1>} />
           </Routes>
         </div>
         <div className="col-3">
